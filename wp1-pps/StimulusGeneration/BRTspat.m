0e0a23ecc782cb63e933f5eb34b71aa6c37c0ad9
kovacs-petra <151035905+kovacs-petra@users.noreply.github.com>
2025-01-21 15:12:56 +0100

    BRT is used to generate cues (but not targets)

--- wp1-pps/StimulusGeneration/BRTspat.m
+++ wp1-pps/StimulusGeneration/BRTspat.m
@@ -1,4 +1,4 @@
-function BRTspat(source, totalDur, azi, ele, r, savename, updateRate, u)
+function cueSpat = BRTspat(cue, totalDur, aziC, eleC, rC, savenameC, updateRate, u)
 
 % Spatialize stimuli by sending OSC commands to the BRT App.
 % Required:
@@ -6,54 +6,46 @@ function BRTspat(source, totalDur, azi, ele, r, savename, updateRate, u)
 % - BRT Renderer Application 
 %
 % Inputs:
-% source        - path for a .wav file to be spatialized
+% cue, target   - paths for cue and target .wav files to be spatialized
 % totalDur      - duration of the sound stimulus in s
-% azi           - azimuth trajectory vector
-% ele           - elevation trajectory vector
-% r             - distance trajectory vector
-% savename      - name of the .mat file to save the results into
-% updateRate    - at what should the location be updated (ms)
+% aziC, T       - azimuth trajectory vector for the Cue and the Target
+% eleC, T       - elevation trajectory vector
+% rC, rT        - distance trajectory vector for the Cue and the Target
+% savenameC, T  - name of the .mat files to save the Cue and Target into
+% updateRate    - at what rate should the location be updated (ms)
 % u             - udpport to use for the communication with BRT
 
-%% Load source
+%% Load and record cue
 oscsend(u, '/removeAllSources', 's', '');
-oscsend(u, '/source/loadSource', 'sss', 'SoundSource1', source, 'DirectivityModel'); 
+oscsend(u, '/source/loadSource', 'sss', 'Cue', cue, 'DirectivityModel');
 
-%% Send out dummy location updates for later timing precision
 % Set trajectory
-testSteps = 12000; 
-testAzi = deg2rad(linspace(90,90,testSteps)); 
-testEle = deg2rad(linspace(0,0,testSteps)); 
-testDist = linspace(2,0.2,testSteps); 
-[xT,yT,zT] = sph2cart(testAzi,testEle,testDist);
-
-for update = 1:testSteps
-    oscsend(u, '/source/location', 'sfff', 'SoundSource1', xT(update), yT(update), zT(update));
-end
-
-%% Set real trajectory
-[x,y,z] = sph2cart(azi,ele,r);
+[xC,yC,zC] = sph2cart(deg2rad(aziC),deg2rad(eleC),rC);
 steps = round(totalDur/updateRate);
 
-for j = 1:4 % The first iterations don't catch the beginning distance precisely enough, but the rest pretty much does
+for j = 1:3 % The first iterations don't catch the beginning distance precisely enough, but the rest pretty much does
     % Set start location
     start = 1;
-    oscsend(u, '/source/location', 'sfff', 'SoundSource1', x(start), y(start), z(start));
+    oscsend(u, '/source/location', 'sfff', 'Cue', xC(start), yC(start), zC(start));
     pause(1); % 1 s delay
 
     % Start recording, then rapidly change location in a for loop
-    oscsend(u, '/source/playAndRecord', 'sssf', 'SoundSource1', savename, 'mat', totalDur);
+    oscsend(u, '/source/playAndRecord', 'sssf', 'Cue', savenameC, 'mat', totalDur);
 
     startTime = tic;
     for i = start+1 : steps
-        oscsend(u, '/source/location', 'sfff', 'SoundSource1', x(i), y(i), z(i));
+        oscsend(u, '/source/location', 'sfff', 'Cue', xC(i), yC(i), zC(i));
         elapsedTime = toc(startTime);
         targetTime = i * updateRate;
         if elapsedTime < targetTime
             pause(targetTime - elapsedTime);
         end
     end
-    pause(1); % maybe then it crashes less often
 end
 oscsend(u, '/stop', 's', '');
+
+%% Create output
+load(savenameC,"Data");
+cueSpat = Data.Receiver;
+
 
