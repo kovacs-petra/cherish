1c1f62ae6ec645051bcd2ef1ac95384aee5c3b3b
kovacs-petra <151035905+kovacs-petra@users.noreply.github.com>
2025-01-22 14:31:35 +0100

    Rationalized the method for ensuring timing precision (cutting the stimulus in a later step rather than repeating the same step more than once)

--- wp1-pps/StimulusGeneration/BRTspat.m
+++ wp1-pps/StimulusGeneration/BRTspat.m
@@ -1,4 +1,4 @@
-function cueSpat = BRTspat(cue, totalDur, aziC, eleC, rC, savenameC, updateRate, u)
+function cueSpat = BRTspat(cue, totalDur, azi, ele, r, savename, updateRate, u)
 
 % Spatialize stimuli by sending OSC commands to the BRT App.
 % Required:
@@ -6,46 +6,45 @@ function cueSpat = BRTspat(cue, totalDur, aziC, eleC, rC, savenameC, updateRate,
 % - BRT Renderer Application 
 %
 % Inputs:
-% cue, target   - paths for cue and target .wav files to be spatialized
-% totalDur      - duration of the sound stimulus in s
-% aziC, T       - azimuth trajectory vector for the Cue and the Target
-% eleC, T       - elevation trajectory vector
-% rC, rT        - distance trajectory vector for the Cue and the Target
-% savenameC, T  - name of the .mat files to save the Cue and Target into
+% cue   	    - path for the .wav file to be spatialized
+% totalDur      - duration of the sound stimulus (cue) in s
+% azi           - azimuth trajectory vector 
+% ele           - elevation trajectory vector
+% r             - distance trajectory vector 
+% savename      - name of the .mat files to save the cue into
 % updateRate    - at what rate should the location be updated (ms)
 % u             - udpport to use for the communication with BRT
 
 %% Load and record cue
 oscsend(u, '/removeAllSources', 's', '');
-oscsend(u, '/source/loadSource', 'sss', 'Cue', cue, 'DirectivityModel');
+oscsend(u, '/source/loadSource', 'sss', 'Cue', cue, 'DirectivityModel'); 
 
 % Set trajectory
-[xC,yC,zC] = sph2cart(deg2rad(aziC),deg2rad(eleC),rC);
+[x,y,z] = sph2cart(deg2rad(azi),deg2rad(ele),r);
 steps = round(totalDur/updateRate);
 
-for j = 1:3 % The first iterations don't catch the beginning distance precisely enough, but the rest pretty much does
-    % Set start location
-    start = 1;
-    oscsend(u, '/source/location', 'sfff', 'Cue', xC(start), yC(start), zC(start));
-    pause(1); % 1 s delay
+% Set start location
+start = 1;
+oscsend(u, '/source/location', 'sfff', 'Cue', x(start), y(start), z(start));
+pause(1); % 1 s delay
 
-    % Start recording, then rapidly change location in a for loop
-    oscsend(u, '/source/playAndRecord', 'sssf', 'Cue', savenameC, 'mat', totalDur);
+% Start recording, then rapidly change location in a for loop
+oscsend(u, '/source/playAndRecord', 'sssf', 'Cue', savename, 'mat', totalDur);
 
-    startTime = tic;
-    for i = start+1 : steps
-        oscsend(u, '/source/location', 'sfff', 'Cue', xC(i), yC(i), zC(i));
-        elapsedTime = toc(startTime);
-        targetTime = i * updateRate;
-        if elapsedTime < targetTime
-            pause(targetTime - elapsedTime);
-        end
+startTime = tic;
+for i = start+1 : steps
+    oscsend(u, '/source/location', 'sfff', 'Cue', x(i), y(i), z(i));
+    elapsedTime = toc(startTime);
+    targetTime = i * updateRate;
+    if elapsedTime < targetTime
+        pause(targetTime - elapsedTime);
     end
 end
+pause(1);
 oscsend(u, '/stop', 's', '');
 
 %% Create output
-load(savenameC,"Data");
+load(savename,"Data");
 cueSpat = Data.Receiver;
 
 
